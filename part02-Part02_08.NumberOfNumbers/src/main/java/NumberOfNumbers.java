860d230bb4984032c0f7ec886a46df18766e166b
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-07 19:08:28 +0200

    completed part02 08_NumberOfNumbers

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -6,5 +6,20 @@ public class NumberOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int attempts = 0;
+        
+        while (true){
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0){
+                System.out.println("Number of numbers: " + attempts);
+                break;
+                
+            }
+            // incrimenting after the conditional
+            //Don't want to do maths inside the println()
+            attempts ++;
+        } 
     }
 }
