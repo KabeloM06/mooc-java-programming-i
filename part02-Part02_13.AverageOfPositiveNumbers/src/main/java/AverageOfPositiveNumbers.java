eaf6bf7617fc4acbf35aa13c82c4ba6b14ecc2cb
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-08 09:41:56 +0200

    Completed part02 13.AverageOfPositiveNumbers

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,29 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int counter = 0;
+        int sum = 0;
+        
+        while (true){
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            // Test if the while loop should continue
+            if (number == 0){
+                break;
+            }
+            // Look for positive numbers
+            if (number > 0){
+                counter++;
+                sum += number;
+            }
+        }
+        
+        //Check if there have been positives
+        if (counter > 0){
+            // Print average
+            System.out.println(1.0*sum/counter);
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
     }
 }
