934db5364fc3bbacc3d74043f6ed283a7db082d2
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-08 10:53:23 +0200

    completed part02 14.Counting

--- part02-Part02_14.Counting/src/main/java/Counting.java
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -6,5 +6,17 @@ public class Counting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num = Integer.valueOf(scanner.nextLine());
+        /*
+        for (int i = 0; i <= num; i++){
+            System.out.println(i);
+        }*/
+        
+        // While loop
+        int i = 0;
+        while (i <= num){
+            System.out.println(i);
+            i++;
+        }
     }
 }
