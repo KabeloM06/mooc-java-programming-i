a6e79382ae51f467f3ee453d5d2b68173df265e6
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-08 11:11:53 +0200

    completed part02 15.CountingToHundred

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,11 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num = Integer.valueOf(scanner.nextLine());
+        int hundred = 100;
+        
+        for (int i = num; i <= hundred; i++ ){
+            System.out.println(i);
+        }
     }
 }
