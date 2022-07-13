844aafef8fcbe0595ddbfb81d08f73d33a9771cb
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-13 17:05:14 +0200

    completed part02 20.RepeatingBreakingAndRememberin

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -14,5 +14,38 @@ public class RepeatingBreakingAndRemembering {
         
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give numbers:");
+        //int value = Integer.valueOf(scanner.nextLine());
+        int sum = 0;
+        int counter = 0;
+        int even = 0;
+        int odd = 0;
+        
+        while (true){
+            int value = Integer.valueOf(scanner.nextLine());
+            // check if loop should end
+            if (value == -1){
+                System.out.println("Thx! Bye!");
+                break;
+            }
+            
+            // Check for even numbers
+            if (value % 2 == 0){
+                even++;
+            } else {
+                // inciment odd
+                odd++;
+            }
+            
+            // Add unputed numbers
+            sum += value;
+            counter++;
+            
+        } 
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + counter);
+        System.out.println("Average: " + 1.0*sum/counter);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
     }
 }
