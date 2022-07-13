72f1ef05983b2338793b59119542fc2b2b31e897
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-14 00:20:01 +0200

    completed part02 26.DivisibleByThree

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -6,6 +6,23 @@ public class DivisibleByThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        divisibleByThreeInRange(3, 6);
+    }
+    
+    // method to check if divisible by 3
+    public static void divisibleByThreeInRange(int beginning, int end){
+        int counter = beginning;
+        
+        while(true){
+            if(counter > end){
+                break;
+            }
+            if(counter % 3 == 0){
+                System.out.println(counter);
+                
+            }
+            counter++;
+        }
     }
 
 }
