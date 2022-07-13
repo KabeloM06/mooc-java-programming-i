4e564f67336be00231bd484219d13b91412a8eb7
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-13 21:48:01 +0200

    completed part02 24.FromParametertoOne

--- part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -3,8 +3,21 @@
 public class FromParameterToOne {
 
     public static void main(String[] args) {
+        printFromNumberToOne(5);
 
-
+    }
+    
+    public static void printFromNumberToOne(int number){
+        
+        int counter = number;
+        
+        while(true){
+            if(counter == 0){
+                break;
+            }
+            System.out.println(counter);
+            counter--;
+        }
     }
 
 }
