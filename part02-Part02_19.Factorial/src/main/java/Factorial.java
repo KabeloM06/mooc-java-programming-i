8355199a0ff8c66894c815cc0636f60d88189e72
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-13 15:31:05 +0200

    completed part02 19.Factorial

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,19 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int factorial = 1;
+        
+        System.out.println("Give a number: ");
+        int number = Integer.valueOf(scanner.nextLine());
+        
+        if (number == 0){
+            factorial = 1;
+        } else {
+            for (int i = 1; i <= number; i++){
+                factorial = factorial*i;
+            }
+        }
+        
+        System.out.println("Factorial: " + factorial);
     }
 }
