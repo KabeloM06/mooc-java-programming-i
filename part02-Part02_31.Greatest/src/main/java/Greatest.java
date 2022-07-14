9a6733280951293e77cb5eee1b9471286efeb64d
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-14 17:23:38 +0200

    completed part02 31.Greatest

--- part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -3,8 +3,18 @@ public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
         //write some code here
-
-        return -1;
+        int highest = number1;
+        if (number1 > highest){
+            highest = number1;
+        }
+        if (number2 > highest){
+            highest = number2;
+        }
+        if (number3 > highest){
+            highest = number3;
+        }
+        
+        return highest;
     }
 
     public static void main(String[] args) {
