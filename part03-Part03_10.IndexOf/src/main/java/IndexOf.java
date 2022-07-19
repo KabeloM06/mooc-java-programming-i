312e4cb560d44e5681b0b97f56c0a0346737651f
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-19 14:29:01 +0200

    completed part03 10.IndexOf

--- part03-Part03_10.IndexOf/src/main/java/IndexOf.java
+++ part03-Part03_10.IndexOf/src/main/java/IndexOf.java
@@ -20,5 +20,13 @@ public class IndexOf {
         System.out.println("");
 
         // implement here finding the indices of a number
+        System.out.println("Search for? ");
+        int value = Integer.valueOf(scanner.nextLine());
+        
+        for (int i = 0; i < list.size(); i++){
+            if (value == list.get(i)){
+                System.out.println(value + " is at index " + i);
+            }
+        }
     }
 }
