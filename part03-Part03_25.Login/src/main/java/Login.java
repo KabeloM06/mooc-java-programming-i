db6ac8c2b3c47b27ec9bedf330785cc18b1b62c9
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-21 14:45:58 +0200

    completed part03 25.Login

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -5,6 +5,20 @@ public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //Enter the username and password
+        System.out.println("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
+        
+        // Check for alex
+        if(username.equals("alex") && password.equals("sunshine")){
+            System.out.println("You have successfully logged in!");
+        } else if (username.equals("emma") && password.equals("haskell")){
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
 
     }
 }
