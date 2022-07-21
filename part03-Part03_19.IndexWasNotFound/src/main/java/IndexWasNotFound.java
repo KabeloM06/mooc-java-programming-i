388c13be9ceccee22474ea0b12737cb90dd17222
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-21 13:13:28 +0200

    completed part03 19.IndexWasNotFound

--- part03-Part03_19.IndexWasNotFound/src/main/java/IndexWasNotFound.java
+++ part03-Part03_19.IndexWasNotFound/src/main/java/IndexWasNotFound.java
@@ -16,10 +16,43 @@ public class IndexWasNotFound {
         array[6] = 9;
         array[7] = 7;
 
-        System.out.print("Search for? ");
+        //System.out.print("Search for? ");
         int searching = Integer.valueOf(scanner.nextLine());
 
-        // Implement the search functionality here
+         //Implement the search functionality here
+        int index = 0;
+        int found = 0;
+        
+        while (index < array.length){
+            //add conditions
+            if (searching == array[index]){
+                found++;
+                System.out.println(searching + " is at index " + index + ".");
+            }
+            
+            index++;
+            
+        
+        }
+        
+        //System.out.println("working");
+        if (found == 0){
+            System.out.println(searching + " was not found.");
+        }
+        
+//        Boolean isPresent = false;
+//        
+//        for (int i = 0; i < array.length; i++){
+//            if (searching == array[i]){
+//                isPresent = true;
+//                System.out.println(searching + " is at index "+ i + ".");
+//            }
+//               
+//        }
+//        // For when number is not present
+//        if (isPresent = false){
+//            System.out.println(searching + " was not found.");
+//        }
     }
 
 }
