447e2ed81797a99df89832aa4fe36b9bc1f198dc
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-22 11:47:20 +0200

    completed part03 32.PersonalDetails

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,32 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String longest = "";
+        int years = 0;
+        int counter = 0;
+        
+        while (true){
+            //read input
+            String input = scanner.nextLine();
+            // break if empty string
+            if (input.equals("")){
+                break;
+            }
+            
+            //split input into string array
+            String[] parts = input.split(",");
+            years += Integer.valueOf(parts[1]);
+            counter++;
+            
+            // check if oldest
+            if (longest.length() < parts[0].length()){
+               
+                longest = parts[0];
+                
+            }
+        }
+        
+        System.out.println("Longest name: " + longest);
+        System.out.println("Agerage of the birth years: " + (1.0*years/counter));
     }
 }
