722ef7e6a7442e0958aaf8b90a3780b91365bf88
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-22 11:42:20 +0200

    completed part03 30.AgeOfTheOldest

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,29 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        int oldest = 0;
+       
+        
+        while (true){
+            //read input
+            String input = scanner.nextLine();
+            // break if empty string
+            if (input.equals("")){
+                break;
+            }
+            
+            //split input into string array
+            String[] parts = input.split(",");
+            
+            
+            // check if oldest
+            if (oldest < Integer.valueOf(parts[1])){
+                oldest = Integer.valueOf(parts[1]);
+                
+            }
+        }
+        
+        //print oldest
+        System.out.println("Age of the oldest: " + oldest);
     }
 }
