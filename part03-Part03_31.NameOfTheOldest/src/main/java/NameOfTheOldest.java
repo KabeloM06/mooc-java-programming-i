67a22ce04bdd497d168ef395369021dbb5e8cc3d
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-22 11:45:57 +0200

    completed part03 31.NameOfTheOldest

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,31 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        int oldest = 0;
+        String name = "";
+       
+        
+        while (true){
+            //read input
+            String input = scanner.nextLine();
+            // break if empty string
+            if (input.equals("")){
+                break;
+            }
+            
+            //split input into string array
+            String[] parts = input.split(",");
+            
+            
+            // check if oldest
+            if (oldest < Integer.valueOf(parts[1])){
+                oldest = Integer.valueOf(parts[1]);
+                name = parts[0];
+                
+            }
+        }
+        
+        //print oldest
+        System.out.println("Name of the oldest: " + name);
     }
 }
