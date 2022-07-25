d342c633d66620f6adf6a03b30dfa4f7f801d1b4
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-07-25 15:26:16 +0200

    completed part04 04.Room

---
+++ part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,10 @@
+public class Room {
+    private String code;
+    private int seats;
+    
+    //create constructor
+    public Room(String classCode, int numberOfSeats){
+        this.code = classCode;
+        this.seats = numberOfSeats;
+    }
+}
